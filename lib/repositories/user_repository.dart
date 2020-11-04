ce0332b578b0e47c3a2d1725252dd0b3a5e425c9
Daniil Melnikov <dmelnikov@sinergo.ru>
2020-11-05 02:40:23 +0700

    Register error handling

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -12,13 +12,29 @@ class UserRepository {
         email: email, password: password);
   }
 
-  Future<void> signUp(String name, String email, String password) async {
-    UserCredential credential = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
+  Future<List<String>> signUp(String name, String email, String password) async {
+    String code, message;
+    try {
+      UserCredential credential = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseFirestore.instance.collection('users').doc(credential.user.uid).set({ 'name': name });
+      message = 'Пользователь успешно зарегистрирован.';
+      code = 'success';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        message = 'Длина пароля должна быть не меньше 6 символов.';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'Аккаунт с таким email уже существует.';
+      } else {
+        message = 'Произошел сбой: ${e.message}';
+      }
+      code = e.code;
+    } catch (e) {
+      message = 'Что то пошло не так.';
+      code = 'undefined';
+    }
 
-    FirebaseFirestore.instance.collection('users').doc(credential.user.uid).set({ 'name': name });
-
-    return credential;
+    return [code, message];
   }
 
   Future<void> signOut() async {
